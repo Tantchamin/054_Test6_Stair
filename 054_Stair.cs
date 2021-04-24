f41d0b9cda606a2d5fe63fb7be1ca2649adfb4e0
Tantchamin <tantchamin@gmail.com>
2021-04-24 16:00:36 +0700

    054 Test6 Heaven Stair

---
+++ 054_Stair.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Test6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Character = char.Parse(Console.ReadLine());
+            int wide = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int row = int.Parse(Console.ReadLine());
+
+            for (int k = 0; k <= row; k++)
+            {
+                for (int z = 0; z < height; z++)
+                {
+                    for (int i = 0; i < k; i++)
+                    {
+                        for (int j = 0; j < wide; j++)
+                        {
+                            Console.Write(Character);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
